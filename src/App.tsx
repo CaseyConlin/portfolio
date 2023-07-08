e4b88fb4fc5a0e4efd608288e039854117914bd1
Casey Conlin <101153261+CaseyConlin@users.noreply.github.com>
2023-07-07 22:14:24 -0400

    Scaffold navbar.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 import { WordGame } from "./Components/WordGame/WordGameContainer";
+import { Navbar } from "./Components/Navbar";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
+        <Navbar />
         <WordGame />
       </ThemeProvider>
     </>
---
+++ src/Components/CCLogo.tsx
@@ -0,0 +1,130 @@
+import SvgIcon from "@mui/material/SvgIcon";
+import { SvgIconProps } from "@mui/material/SvgIcon";
+
+export const CCLogo = (props: SvgIconProps) => {
+  return (
+    <SvgIcon {...props}>
+      <svg width="24" height="24" viewBox="0 0 24 24" xmlSpace="preserve">
+        <g
+          transform="matrix(0.6727095046 0 0 0.6727095046 12 12)"
+          id="XOGLKM4yk8hdqAcecJGbs"
+        >
+          <path
+            style={{
+              strokeLinejoin: "miter",
+              strokeMiterlimit: 4,
+              fill: " rgb(239,83,80)",
+              fillRule: "nonzero",
+              opacity: 1,
+            }}
+            vectorEffect="non-scaling-stroke"
+            transform=" translate(0, 0)"
+            d="M 0 -17.83831 C 9.84675 -17.83831 17.83831 -9.84675 17.83831 0 C 17.83831 9.84675 9.84675 17.83831 0 17.83831 C -9.84675 17.83831 -17.83831 9.84675 -17.83831 0 C -17.83831 -9.84675 -9.84675 -17.83831 0 -17.83831 z"
+            strokeLinecap="round"
+          />
+        </g>
+        <g
+          transform="matrix(1 0 0 1 9.2175716593 14.1463998385)"
+          id="hoHG-34awoSypLohxyyv9"
+        >
+          <text
+            xmlSpace="preserve"
+            fontFamily="Bad Script"
+            fontSize="14"
+            fontStyle="normal"
+            fontWeight="normal"
+            letterSpacing=".07"
+            line-height="1"
+            style={{
+              strokeLinejoin: "miter",
+              strokeMiterlimit: 4,
+              fill: " rgb(0,0,0)",
+              fillRule: "nonzero",
+              opacity: 1,
+            }}
+          >
+            <tspan x="-5.5" y="1.80796">
+              C
+            </tspan>
+          </text>
+          <style></style>
+        </g>
+        <g
+          transform="matrix(1 0 0 1 9.5789722496 15.0214654962)"
+          id="C3nQgcb1HUvnUN-IyTe5K"
+        >
+          <text
+            xmlSpace="preserve"
+            fontFamily="Bad Script"
+            fontSize="15"
+            fontStyle="normal"
+            fontWeight="normal"
+            letterSpacing="0.075"
+            line-height="1"
+            style={{
+              strokeLinejoin: "miter",
+              strokeMiterlimit: 4,
+              fill: " rgb(255,255,255)",
+              fillRule: "nonzero",
+              opacity: 1,
+            }}
+          >
+            <tspan x="-6.5" y="1.1871">
+              C
+            </tspan>
+          </text>
+        </g>
+        <g
+          transform="matrix(1 0 0 1 16.9402008192 16.7388655769)"
+          id="mEAvyM7Wye-soxbCT4kgn"
+        >
+          <text
+            xmlSpace="preserve"
+            fontFamily="Bad Script"
+            fontSize="17"
+            fontStyle="normal"
+            fontWeight="normal"
+            letterSpacing=".085"
+            line-height="1"
+            style={{
+              strokeLinejoin: "miter",
+              strokeMiterlimit: 4,
+              fill: " rgb(0,0,0)",
+              fillRule: "nonzero",
+              opacity: 1,
+            }}
+          >
+            <tspan x="-5.5" y="4.94538">
+              C
+            </tspan>
+          </text>
+        </g>
+        <g
+          transform="matrix(1 0 0 1 16.4019435859 15.9270329196)"
+          id="L6tAvGL_BUNUrZUYE27ND"
+        >
+          <text
+            xmlSpace="preserve"
+            fontFamily="Bad Script"
+            fontSize="18"
+            fontStyle="normal"
+            fontWeight="normal"
+            letterSpacing=".09"
+            line-height="1"
+            style={{
+              strokeLinejoin: "miter",
+              strokeMiterlimit: 4,
+              fill: " rgb(255,255,255)",
+              fillRule: "nonzero",
+              opacity: 1,
+            }}
+          >
+            <tspan x="-5.5" y="5.82452">
+              C
+            </tspan>
+          </text>
+        </g>
+      </svg>
+    </SvgIcon>
+  );
+};
